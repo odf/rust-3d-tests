d2a26fc88a6ecec62c44600f36aeb921aeb34ed0
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2025-05-18 10:47:20 +1000

    refactor the instance generation code

--- examples/instancing.rs
+++ examples/instancing.rs
@@ -91,28 +91,13 @@ fn build_mesh(context: &three_d::Context)
 {
     let mut result = vec![];
 
-    let mut struts = three_d::Gm::new(
-        three_d::InstancedMesh::new(
-            &context,
-            &three_d::Instances::default(),
-            &three_d::CpuMesh::cylinder(24)
-        ),
-        three_d::PhysicalMaterial {
-            albedo: three_d::Srgba::GREEN,
-            metallic: 0.0,
-            roughness: 0.5,
-            ..Default::default()
-        }
-    );
-
-    let center = Mat4::from_translation(vec3(-1.0, 0.0, 0.0))
-        * Mat4::from_nonuniform_scale(2.0, 0.1, 0.1);
+    let shift = |x, y, z| Mat4::from_translation(vec3(x, y, z));
 
     let rot_y = Mat4::from_angle_y(three_d::degrees(90.0));
     let rot_z = Mat4::from_angle_z(three_d::degrees(90.0));
-    let shift = |x, y, z| Mat4::from_translation(vec3(x, y, z));
+    let center = shift(-1.0, 0.0, 0.0) * Mat4::from_nonuniform_scale(2.0, 0.1, 0.1);
 
-    struts.set_instances(&three_d::Instances {
+    let strut_instances = three_d::Instances {
         transformations: vec![
             shift(0.0, -1.0, -1.0) * center,
             shift(0.0, -1.0,  1.0) * center,
@@ -130,27 +115,25 @@ fn build_mesh(context: &three_d::Context)
             rot_z * shift(0.0,  1.0,  1.0) * center,
         ],
         ..Default::default()
-    });
+    };
 
-    result.push(struts);
-
-    let mut balls = three_d::Gm::new(
+    result.push(three_d::Gm::new(
         three_d::InstancedMesh::new(
             &context,
-            &three_d::Instances::default(),
-            &three_d::CpuMesh::sphere(24)
+            &strut_instances,
+            &three_d::CpuMesh::cylinder(24)
         ),
         three_d::PhysicalMaterial {
-            albedo: three_d::Srgba::RED,
+            albedo: three_d::Srgba::GREEN,
             metallic: 0.0,
             roughness: 0.5,
             ..Default::default()
         }
-    );
+    ));
 
     let scale = Mat4::from_scale(0.105);
 
-    balls.set_instances(&three_d::Instances {
+    let ball_instances = three_d::Instances {
         transformations: vec![
             shift(-1.0, -1.0, -1.0) * scale,
             shift(-1.0, -1.0,  1.0) * scale,
@@ -162,9 +145,21 @@ fn build_mesh(context: &three_d::Context)
             shift( 1.0,  1.0,  1.0) * scale,
         ],
         ..Default::default()
-    });
+    };
 
-    result.push(balls);
+    result.push(three_d::Gm::new(
+        three_d::InstancedMesh::new(
+            &context,
+            &ball_instances,
+            &three_d::CpuMesh::sphere(24)
+        ),
+        three_d::PhysicalMaterial {
+            albedo: three_d::Srgba::RED,
+            metallic: 0.0,
+            roughness: 0.5,
+            ..Default::default()
+        }
+    ));
 
     result
 }
